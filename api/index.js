13fecf3ac2a20cdcb4996db1f168e7af1526600a
nasa (Pol Aguilar) <aguilarpol99@gmail.com>
2025-09-07 12:41:35 +0200

    vercel SSR ready

---
+++ api/index.js
@@ -0,0 +1,1 @@
+export default import('../dist/polfolio/server/server.mjs').then((module) => module.app);
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { join } from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
-const app = express();
+export const app = express();
 const angularApp = new AngularNodeAppEngine();
 
 /**
@@ -41,9 +41,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
